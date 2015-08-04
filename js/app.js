642d87d0f91f389ed0f648f30d757ad020131b0e
Nate Christman <nchristman@expedia.com>
2015-08-04 15:46:02 -0700

    The player is now moving when using the keyboard arrows. Need to dial in the exact number of pixels to move

--- js/app.js
+++ js/app.js
@@ -21,31 +21,54 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+/* ====================== Player class below ============================ */
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {}
+var Player = function(x, y) {
+    // This char could be one of many. Might be interesting to randomize the character on game reset/init
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+}
 
-Player.prototype.update = function() {}
-Player.prototype.render = function() {}
-Player.prototype.handleInput = function() {}
+Player.prototype.update = function(dt) {}
+Player.prototype.render = function(x, y) {
+    // Start the player in the middle column, bottom row
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+Player.prototype.handleInput = function(keyCode) {
+    if (keyCode === 'left' && this.x > 0) {
+        // player.render(-100, 0);
+        this.x -= 100;
+    }
+    if (keyCode === 'right' && this.x < 400) {
+        this.x += 100;
+    }
+    if (keyCode === 'up' && this.y > 0) {
+        this.y -= 85;
+    }
+    if (keyCode === 'down' && this.y < 400) {
+        this.y += 85;
+    }
+    console.log(this.x, this.y);
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var player,
-    allEnemies; 
+    allEnemies;
 
-player = new Player();
+player = new Player(200, 400);
 allEnemies = [];
 
 for (var i = 0; i < 6; i++) {
     var newEnemy = new Enemy();
     allEnemies.push(newEnemy);
 }
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
