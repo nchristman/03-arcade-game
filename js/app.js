f94912cd26a4d175dd7624fc68022f32db62eab6
Nate Christman <nchristman@expedia.com>
2015-08-21 09:55:48 -0700

    Added collision detection and added to the player.update function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+var checkCollisions = function(player, enemy) {
+    var enemyPosition = {
+        left: enemy.x,
+        right: enemy.x + 100,
+        top: enemy.y + 77,
+        bottom: enemy.y + 154
+    }
+    var playerPosition = {
+        left: player.x,
+        right: player.x + 100,
+        top: player.y + 64,
+        bottom: player.y + 152
+    }
+
+    // Create an offset to deal with the transparent top of the sprite causing collisions
+    var offset = 50;
+
+    // Reset the player if the collision is detected
+    if (enemyPosition.left < playerPosition.right &&
+        enemyPosition.right > playerPosition.left &&
+        enemyPosition.top < playerPosition.bottom &&
+        enemyPosition.bottom > playerPosition.top + offset) {
+        player.resetPosition();
+    }
+}
+
 // Enemies our player must avoid
 var Enemy = function(startX, startY, speed) {
     // Variables applied to each of our instances go here, we've provided one for you to get started
@@ -58,7 +84,11 @@ var Player = function(x, y) {
 }
 
 Player.prototype.update = function(dt) {
-    // Unsure of what would need to go here? 
+    // Unsure of what would need to go here? It said it was needed?
+    // Check if the player is occupying the same space as any of the enemies
+    for (var i = 0; i < allEnemies.length; i++) {
+        checkCollisions(this, allEnemies[i]);
+    }
 }
 
 Player.prototype.render = function(x, y) {
@@ -91,7 +121,6 @@ Player.prototype.handleInput = function(keyCode) {
     if (keyCode === 'down' && this.y < canvasBottomLimit) {
         this.y += playerMovementY;
     }
-    // console.log(this.x, this.y);
 }
 
 // Now instantiate your objects.
